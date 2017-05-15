7172b59bde937c07fe0f722b5d515410c47babcc
shadySource <shadysource2@gmail.com>
2017-05-15 14:23:59 -0400

    add mnist_generator

--- cyclegan/models.py
+++ cyclegan/models.py
@@ -1,12 +1,12 @@
 '''
 source for models
 '''
-from keras.layers import Conv2D
+from keras.layers import Conv2D, UpSampling2D, Input
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.merge import Add
 from keras.models import Model
 
-from .instance_norm import InstanceNormalization
+from instance_norm import InstanceNormalization
 
 def conv_block(x0, scale):
     x = Conv2D(int(64*scale), (1, 1))(x0)
@@ -48,3 +48,22 @@ def residual_block(x0, scale=1, num_id=2):
     for i in range(num_id):
         x0 = identity_block(x0, scale)
     return x0
+
+def mnist_generator(input_shape=(28, 28, 1), scale=1/4):
+    x0 = Input(input_shape)
+    x = Conv2D(int(128*scale), (3, 3), strides=(2, 2), padding='same')(x0)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(int(64*scale), (3, 3), strides=(2, 2), padding='same')(x)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = residual_block(x, scale, num_id=2)
+    x = residual_block(x, scale*2, num_id=3)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(int(128*scale), (1, 1))(x)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = UpSampling2D(size=(2, 2))(x)
+    x = Conv2D(1, (1, 1), activation='linear')(x)
+    # print(x.shape)
+    return Model(x0, x)
