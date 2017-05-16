c18c61438061c64793cc5c0506c0336139a463fc
shadySource <shadysource2@gmail.com>
2017-05-16 16:52:52 -0400

    update models and losses

--- cyclegan/losses.py
+++ cyclegan/losses.py
@@ -3,20 +3,22 @@ Loss functions for CycleGan
 '''
 import keras.backend as k
 from keras.losses import mean_squared_error
-_discriminator_train_thresh = 0.2
 
-def descriminator_loss(y_true, y_pred):
+def discriminator_loss(y_true, y_pred):
     loss = mean_squared_error(y_true, y_pred)
-    is_large = k.greater(loss, _discriminator_train_thresh) # threshold
-    return loss * is_large
+    is_large = k.greater(loss, k.constant(0.2)) # threshold
+    is_large = k.cast(is_large, k.floatx())
+    return loss * is_large # binary threshold the loss to prevent overtraining the discriminator
 
 def cycle_loss(y_true, y_pred):
     if k.image_data_format() is 'channels_first':
-        x_w, x_h = (2, 3)
+        x_w = 2
+        x_h = 3
     else:
-        x_w, x_h = (1, 2)
-    loss = k.abs(k.max((y_true - y_pred, k.epsilon)))
-    loss = k.sum(loss, x_h)
-    loss = k.sum(loss, x_w)
+        x_w = 1
+        x_h = 2
+    loss = k.abs(y_true - y_pred)
+    loss = k.sum(loss, axis=x_h)
+    loss = k.sum(loss, axis=x_w)
     return loss
 
--- cyclegan/models.py
+++ cyclegan/models.py
@@ -6,7 +6,7 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.merge import Add
 from keras.models import Model
 
-from instance_norm import InstanceNormalization
+from .instance_norm import InstanceNormalization
 
 def conv_block(x0, scale):
     x = Conv2D(int(64*scale), (1, 1))(x0)
@@ -65,5 +65,23 @@ def mnist_generator(input_shape=(28, 28, 1), scale=1/4):
     x = LeakyReLU()(x)
     x = UpSampling2D(size=(2, 2))(x)
     x = Conv2D(1, (1, 1), activation='linear')(x)
-    # print(x.shape)
+    return Model(x0, x)
+
+def mnist_discriminator(input_shape=(28, 28, 1), scale=1/4):
+    x0 = Input(input_shape)
+    x = Conv2D(int(128*scale), (3, 3), strides=(2, 2), padding='same')(x0)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(int(64*scale), (3, 3), strides=(2, 2), padding='same')(x)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = residual_block(x, scale, num_id=2)
+    x = residual_block(x, scale*2, num_id=3)
+    x = Conv2D(int(128*scale), (3, 3), strides=(2, 2), padding='same')(x)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(int(128*scale), (3, 3), strides=(2, 2), padding='same')(x)
+    x = InstanceNormalization()(x)
+    x = LeakyReLU()(x)
+    x = Conv2D(1, (3, 3), strides=(2, 2), padding='same', activation='sigmoid')(x)
     return Model(x0, x)
