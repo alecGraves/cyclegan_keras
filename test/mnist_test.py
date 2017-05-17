13d0f05c189f4cdb2757a06f00c85151ddcc8a8d
shadySource <shadysource2@gmail.com>
2017-05-16 23:51:49 -0400

    change activations of outputs of mnist gen and disc nets

--- test/mnist_test.py
+++ test/mnist_test.py
@@ -8,6 +8,7 @@ import keras.backend as k
 from keras.layers import Input
 from keras.datasets import mnist
 from keras.models import Model
+from keras.optimizers import Adam
 
 import context
 from cyclegan.models import mnist_discriminator, mnist_generator
@@ -46,18 +47,28 @@ def load_input_images(from_jpegs=False):
         images[i] = Image.open(os.path.join(current_dir,
                                             'images',
                                             filename)).resize((28, 28)).convert('L')
+    images = images.astype('float32')
+    images /= 255
     if k.image_data_format() == 'channels_first':
         images = images.reshape(len(image_names), 1, 28, 28)
     else:
         images = images.reshape(len(image_names), 28, 28, 1)
     return images
 
+def prettify(image):
+    image *= 255
+    image = np.uint8(image)
+    image = np.reshape(image, (28, 28))
+    return image
+
 def test_cyclegan():
     mnist_shape, mnist_images = load_mnist()
     faces = load_input_images()
 
-    nb_epochs = 1
-    batch_size = faces.shape[0]
+    nb_epochs = 100
+    batch_size = 1024
+    adam_lr = 0.0002
+    adam_beta_1 = 0.5
     history_size = int(batch_size * 7/3)
     generation_history_mnist = None
     generation_history_faces = None
@@ -70,10 +81,10 @@ def test_cyclegan():
     generator_mnist = mnist_generator(mnist_shape)
     discriminator_mnist = mnist_discriminator(mnist_shape)
 
-    discriminator_faces.compile(optimizer='Adam', loss=discriminator_loss)
-    discriminator_mnist.compile(optimizer='Adam', loss=discriminator_loss)
-    generator_faces.compile(optimizer='Adam', loss='mean_squared_error')
-    generator_mnist.compile(optimizer='Adam', loss='mean_squared_error')
+    discriminator_faces.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss=discriminator_loss)
+    discriminator_mnist.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss=discriminator_loss)
+    generator_faces.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss='mean_squared_error')
+    generator_mnist.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss='mean_squared_error')
 
     fake_face = generator_faces(mnist_input)
     fake_mnist = generator_mnist(face_input)
@@ -83,11 +94,15 @@ def test_cyclegan():
     discriminator_faces.trainable = False
     discriminator_mnist.trainable = False
 
-    face_gen_trainer = Model(mnist_input, [discriminator_faces(fake_face), generator_mnist(fake_face)])
-    mnist_gen_trainer = Model(face_input, [discriminator_mnist(fake_mnist), generator_faces(fake_mnist)])
+    face_gen_trainer = Model(mnist_input, discriminator_faces(fake_face))
+    mnist_gen_trainer = Model(face_input, discriminator_mnist(fake_mnist))
+    mnist_cyc = Model(mnist_input, generator_mnist(fake_face))
+    faces_cyc = Model(face_input, generator_faces(fake_mnist))
 
-    face_gen_trainer.compile(optimizer='Adam', loss=['mean_squared_error', cycle_loss])
-    mnist_gen_trainer.compile(optimizer='Adam', loss=['mean_squared_error', cycle_loss])
+    face_gen_trainer.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss='mean_squared_error')
+    mnist_gen_trainer.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss='mean_squared_error')
+    mnist_cyc.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss=cycle_loss)
+    faces_cyc.compile(optimizer=Adam(lr=adam_lr, beta_1=adam_beta_1), loss=cycle_loss)
 
     # training time
 
@@ -131,18 +146,26 @@ def test_cyclegan():
             print("Faces Discriminator Loss:", faces_discrim_loss[-1])
 
             # Train generators.
-            cyc_multiplier = 10
+            mnist_gen_loss.append(mnist_gen_trainer.train_on_batch(faces_batch_real, np.ones(batch_size)))
+            print("MNIST Generator Loss:", mnist_gen_loss[-1])
+            faces_gen_loss.append(face_gen_trainer.train_on_batch(mnist_batch_real, np.ones(batch_size)))
+            print("Faces Generator Loss:", faces_gen_loss[-1])
 
-            mnist_gen_loss.append(mnist_gen_trainer.train_on_batch(faces_batch_real,
-                                                                   [np.ones(batch_size), faces_batch_real],
-                                                                   sample_weight=[np.ones(batch_size), np.array([cyc_multiplier]*batch_size)]))
-            print("MNIST Generator Loss:", mnist_gen_loss[-1][1])
-            print("Face(MNIST(face)) Cycle Loss:", mnist_gen_loss[-1][2])
-            faces_gen_loss.append(face_gen_trainer.train_on_batch(mnist_batch_real,
-                                                                  [np.ones(batch_size), mnist_batch_real],
-                                                                  sample_weight=[np.ones(batch_size), np.array([cyc_multiplier]*batch_size)]))
-            print("Faces Generator Loss:", faces_gen_loss[-1][1])
-            print("MNIST(Face(mnist))) Cycle Loss:", mnist_gen_loss[-1][2])
+            cyc_multiplier = 10
+            mnist_cyc_loss.append(mnist_cyc.train_on_batch(mnist_batch_real, mnist_batch_real, sample_weight=np.array([cyc_multiplier]*batch_size)))
+            print("MNIST Cyclic Loss:", mnist_cyc_loss[-1])
+            faces_cyc_loss.append(faces_cyc.train_on_batch(faces_batch_real, faces_batch_real, sample_weight=np.array([cyc_multiplier]*batch_size)))
+            print("Faces Cyclic Loss:", faces_cyc_loss[-1])
+
+        choice = np.random.choice(mnist_images.shape[0])
+        mnist_in = mnist_images[choice].reshape((1, 28, 28, 1))
+        face_out = generator_faces.predict(mnist_in)
+        mnist_in = Image.fromarray(prettify(mnist_in), mode='L')
+        face_out = Image.fromarray(prettify(face_out), mode='L')
+        mnist_in.save(str(epoch)+'_mnist_in.jpg')
+        face_out.save(str(epoch)+'_face_out.jpg')
+
+    generator_faces.save('generator_faces.h5')
 
 if __name__ == "__main__":
     test_cyclegan()
